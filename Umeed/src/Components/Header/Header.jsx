c35ff70a877a95b02152fef0ad4e64c88e7496c2
SHIVAMCHARDE <83965173+SHIVAMCHARDE@users.noreply.github.com>
2023-04-05 00:53:40 +0530

    Header structure created Css remain

---
+++ Umeed/src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../Header/Header.css'
+export default function Header() {
+    return (
+        <div className='HeaderContainer'>
+            <div className="Hname">
+            <p>Umeed</p>
+            </div>
+            <div className="HeaderComponents">
+                <div className="HeaderHome">
+                    <p>Home</p>
+                </div>
+                <div className="HeaderConnect">
+                    <p>Connect</p>
+                </div>
+                <div className="HeaderSelfHelp">
+                    <p>Self help groups</p>
+
+                </div>
+                <div className="Headerprofilepic">
+                    <p>Umeed</p>
+
+                </div>
+            </div>
+        </div>
+    )
+}
