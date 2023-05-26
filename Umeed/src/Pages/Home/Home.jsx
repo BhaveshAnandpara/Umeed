a07c740b3f2e048828d051bd608e1c2fac3b279a
vaishna2002 <co.2020.vabhende@bitwardha.ac.in>
2023-05-26 14:33:15 +0530

    home page changes going on

--- Umeed/src/Pages/Home/Home.jsx
+++ Umeed/src/Pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../Home/Home.css'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='homebox'>
+      <div className='home1'>
+      <p className='home1p'>Hold on this UMEED, you can get better from depression</p>
+      </div>
+      <div className='home2'>
+
+      </div>
+      <div className='home3'>
+
+      </div>
     </div>
   )
 }
