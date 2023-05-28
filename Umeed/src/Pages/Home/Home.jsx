ff68ddb47a7273dc35a733f57862c6312b96ec6e
SHIVAMCHARDE <83965173+SHIVAMCHARDE@users.noreply.github.com>
2023-05-28 18:58:03 +0530

    Home Page Done

--- Umeed/src/Pages/Home/Home.jsx
+++ Umeed/src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import '../Home/Home.css'
 export default function Home() {
   return (
     <div className='homebox'>
-      <div className='home1'>
+      {/* <div className='home1'>
       <p className='home1p'>Hold on this UMEED, you can get better from depression</p>
       </div>
       <div className='home2'>
@@ -12,7 +12,19 @@ export default function Home() {
       </div>
       <div className='home3'>
 
+      </div> */}
+      <div className="ImgSectionOne">
+        <div className='Thougth'>
+          <p className='PThougth' >"Umeed: Discover a Brighter Future"</p>
+        </div>
+      
+
       </div>
+      <div className="HallOfFrame">
+       <div className='HallImgContainer'>
+          <p className='HallHeading'>Wall Of Frame</p>
+        </div>
+       </div>
     </div>
   )
 }
